4ef9ae4b303bcde8a3b99a82714d05b333d53a6b
chauhan-abhi <chauhanabhi1245@gmail.com>
2018-12-18 14:30:55 +0530

    structuring code in packages and ignoring unnecessary entries in response

--- app/src/main/java/com/example/forecastmvvm/data/db/entity/CurrentWeatherEntry.kt
+++ app/src/main/java/com/example/forecastmvvm/data/db/entity/CurrentWeatherEntry.kt
@@ -1,40 +1,28 @@
-package com.example.forecastmvvm.data.response
+package com.example.forecastmvvm.data.db.entity
 
 import com.google.gson.annotations.SerializedName
 
 data class CurrentWeatherEntry(
     val uv: Int,
-    val humidity: Int,
-    val cloud: Int,
     val condition: Condition,
     @SerializedName("feelslike_c")
-    val feelslikeC: Int,
+    val feelslikeC: Double,
     @SerializedName("feelslike_f")
     val feelslikeF: Double,
     @SerializedName("is_day")
     val isDay: Int,
-    @SerializedName("last_updated")
-    val lastUpdated: String,
-    @SerializedName("last_updated_epoch")
-    val lastUpdatedEpoch: Int,
     @SerializedName("precip_in")
-    val precipIn: Int,
+    val precipIn: Double,
     @SerializedName("precip_mm")
-    val precipMm: Int,
-    @SerializedName("pressure_in")
-    val pressureIn: Double,
-    @SerializedName("pressure_mb")
-    val pressureMb: Int,
+    val precipMm: Double,
     @SerializedName("temp_c")
-    val tempC: Int,
+    val tempC: Double,
     @SerializedName("temp_f")
     val tempF: Double,
     @SerializedName("vis_km")
     val visKm: Double,
     @SerializedName("vis_miles")
-    val visMiles: Int,
-    @SerializedName("wind_degree")
-    val windDegree: Int,
+    val visMiles: Double,
     @SerializedName("wind_dir")
     val windDir: String,
     @SerializedName("wind_kph")
--- app/src/main/java/com/example/forecastmvvm/data/network/response/CurrentWeatherResponse.kt
+++ app/src/main/java/com/example/forecastmvvm/data/network/response/CurrentWeatherResponse.kt
@@ -1,5 +1,7 @@
-package com.example.forecastmvvm.data.response
+package com.example.forecastmvvm.data.network.response
 
+import com.example.forecastmvvm.data.db.entity.CurrentWeatherEntry
+import com.example.forecastmvvm.data.db.entity.WeatherLocation
 import com.google.gson.annotations.SerializedName
 
 data class CurrentWeatherResponse(
