7198f83710cf818d46acb02d163de2a2628128d6
chauhan-abhi <chauhanabhi1245@gmail.com>
2018-12-18 15:06:18 +0530

    dao interface and database class created

---
+++ app/src/main/java/com/example/forecastmvvm/data/db/CurrentWeatherDao.kt
@@ -0,0 +1,24 @@
+package com.example.forecastmvvm.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.forecastmvvm.data.db.entity.CURRENT_WEATHER_ID
+import com.example.forecastmvvm.data.db.entity.CurrentWeatherEntry
+import com.example.forecastmvvm.data.db.unitlocalised.ImperialCurrentWeatherEntry
+import com.example.forecastmvvm.data.db.unitlocalised.MetricCurrentWeatherEntry
+
+@Dao
+interface CurrentWeatherDao {
+    // Insert or update
+    @Insert(onConflict  =  OnConflictStrategy.REPLACE)
+    fun upsert(weatherEntry: CurrentWeatherEntry)
+
+    @Query(value = "select * from current_weather where id = $CURRENT_WEATHER_ID")
+    fun getWeatherMetric(): LiveData<MetricCurrentWeatherEntry>
+
+    @Query(value = "select * from current_weather where id = $CURRENT_WEATHER_ID")
+    fun getWeatherImperial(): LiveData<ImperialCurrentWeatherEntry>
+}
---
+++ app/src/main/java/com/example/forecastmvvm/data/db/ForecastDatabase.kt
@@ -0,0 +1,37 @@
+package com.example.forecastmvvm.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.forecastmvvm.data.db.entity.CurrentWeatherEntry
+
+@Database(
+    entities = [CurrentWeatherEntry::class],
+    version = 1
+)
+abstract class ForecastDatabase : RoomDatabase() {
+    abstract fun currentWeatherDao() : CurrentWeatherDao
+
+    // Database needs to be singleton,
+    // no sense in having multiple instances of database at same time
+    companion object {
+        @Volatile private var instance: ForecastDatabase? = null
+        // LOCK is a dummy object to make sure no two threads are
+        // currently doing the same thing working in parallel
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            // also executes after successful completion of buildDatabase function
+            // and initializing instance with the
+            // it{ return value from the function }
+            instance ?: buildDatabase(context).also { instance = it }
+        }
+
+        private fun buildDatabase(context: Context) =
+                Room.databaseBuilder(context.applicationContext,
+                    ForecastDatabase::class.java, "forecast.db")
+                    .build()
+    }
+
+}
